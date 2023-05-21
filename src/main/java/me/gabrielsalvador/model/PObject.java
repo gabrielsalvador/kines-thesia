2981d8e91020f76f349e7863fb0b27dee6efaadd
Gabriel Salvador <gabriel.carvalho.salvador@gmail.com>
2023-05-21 17:48:59 +0100

    pobject properties

--- src/main/java/me/gabrielsalvador/model/PObject.java
+++ src/main/java/me/gabrielsalvador/model/PObject.java
@@ -9,8 +9,44 @@ import processing.core.PVector;
 public class PObject {
     PVector position;
     PVector size;
-    public final Set<PObject> _children = new HashSet<PObject>();
-    HashMap<String, PObjectProperty<?>> properties = new HashMap<String, PObjectProperty<?>>();
+    private final Set<PObject> _children = new HashSet<PObject>();
+    private final HashMap<String, PObjectProperty<?>> _properties = new HashMap<String, PObjectProperty<?>>();
+
+    public PObject() {
+    }
 
 
+    PObject setPosition(PVector position) {
+        this.position = position;
+        return this;
+    }
+
+
+    public PVector getPosition() {
+        return position;
+    }
+
+
+    PObject setSize(PVector size) {
+        this.size = size;
+        return this;
+    }
+
+
+    public PVector getSize() {
+        return size;
+    }
+
+
+    public Set<PObject> addChildren(PObject pObject) {
+        _children.add(pObject);
+        return _children;
+    }
+
+
+
+    
+    public PObjectProperty<?> getProperty(String name) {
+        return _properties.get(name);
+    }
 }
--- src/main/java/me/gabrielsalvador/model/PObjectProperty.java
+++ src/main/java/me/gabrielsalvador/model/PObjectProperty.java
@@ -1,5 +1,27 @@
 package me.gabrielsalvador.model;
 
 public class PObjectProperty<T>{
-    
+    String name;
+    Object value;
+    Class<T> valueType;
+
+    public PObjectProperty(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public PObjectProperty<T> setValue(T value) {
+        this.value = value;
+        return this;
+    }
+    @SuppressWarnings("unchecked")
+    public T getValue() {
+        return (T) value; 
+    }
+
+    public Class<?> getValueType() {
+        return value.getClass();
+    }
 }
