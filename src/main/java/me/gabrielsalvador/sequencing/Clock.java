4f1095257cebb1eb99ebf38093ef5a32ee57f523
Gabriel Salvador <gabriel.carvalho.salvador@gmail.com>
2023-08-07 14:05:27 +0100

    improved clock

--- src/main/java/me/gabrielsalvador/sequencing/Clock.java
+++ src/main/java/me/gabrielsalvador/sequencing/Clock.java
@@ -5,38 +5,48 @@ import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class Clock {
 
     private final List<Device> _devices = new ArrayList<Device>();
     private final ScheduledExecutorService executorService;
     private int _tempo = 120;
 
-
     public Clock() {
         this.executorService = Executors.newSingleThreadScheduledExecutor();
+        startTickExecutor();
     }
 
-
-
     public void setTempo(int tempo) {
-
+        this._tempo = tempo;
+        restartTickExecutor();
     }
 
     private void tick() {
-        for (Device d  :_devices) {
-            d.clock();
+        for (Device d : _devices) {
+            d.clockTick();
         }
-
     }
 
     private int getPeriod() {
-
-        return (60000 / (_tempo * 32));
+        return 60000 / (_tempo * 16);
     }
 
     public void addDevice(Device device) {
-
-
+        _devices.add(device);
     }
 
+    private void startTickExecutor() {
+        executorService.scheduleAtFixedRate(this::tick, 0, getPeriod(), TimeUnit.MILLISECONDS);
+    }
+
+    private void restartTickExecutor() {
+        executorService.shutdownNow();
+        startTickExecutor();
+    }
 }
