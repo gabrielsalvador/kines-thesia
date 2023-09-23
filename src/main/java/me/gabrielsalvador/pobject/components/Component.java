be49946bdf39a79afb035f0a5cb1a6b3c0bbae6e
Gabriel Salvador <gabriel.carvalho.salvador@gmail.com>
2023-09-23 20:29:01 +0100

    cleaned up Component class

--- src/main/java/me/gabrielsalvador/pobject/components/Component.java
+++ src/main/java/me/gabrielsalvador/pobject/components/Component.java
@@ -41,10 +41,7 @@ public abstract class  Component implements Serializable {
     }
 
     public abstract void display(PGraphics graphics) ;
-    public String getName() {
-        // Implementation here
-        return "";
-    }
+    public abstract String getName();
 
 
 
