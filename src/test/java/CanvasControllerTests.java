7c3c5e18e71932628b31b17fdd355bc316dcb375
Gabriel Salvador <gabriel.carvalho.salvador@gmail.com>
2023-10-01 16:52:23 +0100

    made Pointer its own class

--- src/test/java/CanvasControllerTests.java
+++ src/test/java/CanvasControllerTests.java
@@ -1,26 +1,44 @@
 import controlP5.ControlP5;
-import controlP5.ControllerInterface;
+import controlP5.ControlWindow;
+import controlP5.Pointer;
 import me.gabrielsalvador.core.CanvasController;
 import org.junit.jupiter.api.BeforeAll;
 
-import org.mockito.ArgumentCaptor;
+import org.junit.jupiter.api.Test;
 
 import static org.mockito.Mockito.*;
 
 
 public class CanvasControllerTests {
-    private ControlP5 mockCp5;
+    static ControlP5 mockCp5;
+    static int mouseX = 0;
+    static int mouseY = 0;
+
+    static CanvasController canvasController;
 
 
-
-    public void setUp() {
+    @BeforeAll
+    public static void setUp() {
 
         mockCp5 = mock(ControlP5.class);
-        CanvasController canvasController = new CanvasController(mockCp5, "CanvasController");
 
-        canvasController.mousePressed();
+        canvasController = new CanvasController(mockCp5, "CanvasController");
+        when(mockCp5.getPointer()).thenReturn(  );
+
+
+
+
+
+
 
 
     }
 
+    @Test
+    public void shouldSelectPObject() {
+        canvasController.onMove();
+        canvasController.onPress();
+
+
+    }
 }
