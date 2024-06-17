16af9ee4ccefdeabc9689141a96372acde79f42f
Gabriel Salvador <gabriel.carvalho.salvador@gmail.com>
2024-06-17 21:13:31 +0100

    More tests for Kinescript

--- src/test/java/KCommandTests.java
+++ src/test/java/KCommandTests.java
@@ -2,6 +2,7 @@
 
 import me.gabrielsalvador.kinescript.ast.KExpression;
 import me.gabrielsalvador.kinescript.ast.KFunction;
+import me.gabrielsalvador.kinescript.builtins.KRandom;
 import me.gabrielsalvador.kinescript.lang.Kinescript;
 import me.gabrielsalvador.kinescript.lang.KinescriptLexer;
 import me.gabrielsalvador.kinescript.lang.KinescriptParser;
@@ -11,13 +12,16 @@ import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.atn.PredictionMode;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
 import java.time.Duration;
 import java.util.HashMap;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+
+@PrepareForTest(Kinescript.class)
 public class KCommandTests {
 
     @BeforeAll
@@ -120,16 +124,99 @@ public class KCommandTests {
 
         HashMap scope = new HashMap();
         long start = System.nanoTime();
-        program.execute(scope);
+//        program.execute(scope);
         long end = System.nanoTime();
         System.out.println("Execution time: " + Duration.ofNanos(end - start).toMillis() + "ms");
 
 
+    }
+
+
+    @Test
+    public void testOperation() throws Exception {
+        // Create an instance of Kinescript
+        Kinescript kinescript = new Kinescript();
+        KRandom mockKRandom = PowerMockito.mock(KRandom.class);
+        //when mockKRandom.execute() is called, return 1
+        PowerMockito.when(mockKRandom.execute(Mockito.any())).thenReturn(1);
+        Kinescript.addBuiltInFunction("random", mockKRandom);
+
+        String input = "a = random() + random() + random(); print(a)";
+
+        KinescriptLexer lexer = new KinescriptLexer(CharStreams.fromString(input));
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        KinescriptParser parser = new KinescriptParser(tokens);
+        parser.getInterpreter().setPredictionMode(PredictionMode.SLL);
+        KinescriptParser.ProgramContext tree = parser.program();
 
 
 
+        // Use the visitProgram method on the tree
+        KFunction program = (KFunction) kinescript.visitProgram(tree);
 
 
+
+        HashMap scope = new HashMap();
+        long start = System.nanoTime();
+        program.execute(scope);
+        long end = System.nanoTime();
+        System.out.println("Execution time: " + Duration.ofNanos(end - start).toMillis() + "ms");
+
+        //assert that KRandom is called 3 times
+        Mockito.verify(mockKRandom, Mockito.times(3)).execute(Mockito.any());
+
+    }
+
+
+
+    @Test
+    public void testExprCrunch(){
+        String input = "1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10";
+
+        KinescriptLexer lexer = new KinescriptLexer(CharStreams.fromString(input));
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        KinescriptParser parser = new KinescriptParser(tokens);
+        parser.getInterpreter().setPredictionMode(PredictionMode.SLL);
+        KinescriptParser.ProgramContext tree = parser.program();
+
+        // Create an instance of Kinescript
+        Kinescript kinescript = new Kinescript();
+
+        // Use the visitProgram method on the tree
+        KFunction program = (KFunction) kinescript.visitProgram(tree);
+
+        HashMap scope = new HashMap();
+        long start = System.nanoTime();
+        program.execute(scope);
+        KExpression statement = (KExpression) program.getStatements().get(0);
+        assertEquals(55.0, statement.evaluate(scope));
+
+        long end = System.nanoTime();
+        System.out.println("Execution time: " + Duration.ofNanos(end - start).toMillis() + "ms");
+    }
+
+
+    @Test
+    public void testOperations(){
+        String input = "a = 234*193/2+1 ";
+
+        KinescriptLexer lexer = new KinescriptLexer(CharStreams.fromString(input));
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        KinescriptParser parser = new KinescriptParser(tokens);
+        parser.getInterpreter().setPredictionMode(PredictionMode.SLL);
+        KinescriptParser.ProgramContext tree = parser.program();
+
+        Kinescript kinescript = new Kinescript();
+
+        KFunction program = (KFunction) kinescript.visitProgram(tree);
+
+        HashMap scope = new HashMap();
+        long start = System.nanoTime();
+        program.execute(scope);
+        long end = System.nanoTime();
+        System.out.println("Execution time: " + Duration.ofNanos(end - start).toMillis() + "ms");
+
+        assertEquals(22581.0, scope.get("a"));
     }
 
 }
